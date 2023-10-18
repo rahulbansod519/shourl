f1c6361d476d32293431f1912cf6fc64419dce57
Rahul Bansod <rahulbansod519@gmail.com>
2023-10-18 15:56:53 +0000

    create an index.html in each directory

--- index.js
+++ index.js
@@ -13,4 +13,15 @@ const templateHtml = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf
 
 for (let [slug, url] of Object.entries(redirects)){
     console.log('Generating html page for', slug)
+
+    const html = templateHtml.replaceAll('https://example.com',url)
+    // console.log(html)
+
+    //folder for each slug
+    const folderPath = path.join(__dirname, 'out', slug)
+    fs.mkdirSync(folderPath, {recursive: true})
+
+    //create an index.html in each directory
+    fs.writeFileSync(path.join(folderPath, 'index.html'), html)
+    
 }
