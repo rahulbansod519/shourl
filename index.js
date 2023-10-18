b956170fa583b48a9c416dde384be045a2043aba
Rahul Bansod <rahulbansod519@gmail.com>
2023-10-18 15:47:01 +0000

    loop through url

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const YAML = require('yaml')
+const fs = require('fs')
+const path = require('path')
+
+const redirectFile = fs.readFileSync(path.join(__dirname, 'redirect.yaml'), 'utf-8')
+// console.log(redirectFile)
+const redirects = YAML.parse(redirectFile)
+// console.log(redirects)
+
+const templateHtml = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf-8')
+
+//loop through all urls and genrate an html page
+
+for (let [slug, url] of Object.entries(redirects)){
+    console.log('Generating html page for', slug)
+}
